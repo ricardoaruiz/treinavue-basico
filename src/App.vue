7aef41ebcc78dd88c10e4c878f34b4fd9ffb826f
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2019-09-29 16:59:12 -0300

    Resultado

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p :class="quantidade <=0 ? 'vermelho' : 'verde'">iPhone: {{quantidade}}</p>
+    <button @click="decrementarQuantidade">Decrementar quantidade</button>
+    <p>{{mensagem}}</p>    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      quantidade: 5,
+      mensagem: ""
+    }
+  },
+  methods: {
+    decrementarQuantidade() {
+      this.quantidade--;      
+      if ( this.quantidade <= 0 ) {
+        this.mensagem = 'Produto esgotado';
+      }
+    }
   }
 }
 </script>
 
 <style>
+.vermelho {
+  color: red;
+}
+.verde {
+  color: green;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
