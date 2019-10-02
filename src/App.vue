4406a8435a40fa48edfa0f576a6f61c3b0a2c335
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2019-10-02 11:16:50 -0300

    Resultado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <label for="nome">Nome</label>
+      <input type="text" id="nome" name="nome" v-model="nome" />
+      
+      <label for="email">E-mail</label>
+      <input type="email" id="email" name="email" v-model="email" />
+
+      <label for="telefone">Telefone</label>
+      <input type="text" id="telefone" name="telefone" v-model="telefone" />
+      
+      <button @click.prevent="enviar">Enviar</button>
+      <button @click.prevent="limpar">Limpar</button>
+      <div v-if="enviado">{{resultado}}</div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      nome: "",
+      email: "",
+      telefone: "",
+      enviado: false
+    }
+  },
+  computed: {
+    resultado() {
+      return `${this.nome} - ${this.email} - ${this.telefone}`;
+    }
+  },
+  watch: {
+    nome() {this.enviado = false;},
+    email() {this.enviado = false;},
+    telefone() {this.enviado = false;}
+  },
+  methods: {
+    enviar() {
+      this.enviado = true;
+    },
+    limpar() {
+      this.nome = "";
+      this.email = "";
+      this.telefone = "";
+      this.enviado = false;
+    }
   }
 }
 </script>
@@ -24,5 +59,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+form {
+  width: 500px;
+  margin: 0 auto;
+  display: grid;
+}
+label {
+  text-align: left;
+  margin-bottom: 10px;
+}
+input {
+  margin-bottom: 10px;
+}
+button {
+  margin-bottom: 10px;
 }
 </style>
